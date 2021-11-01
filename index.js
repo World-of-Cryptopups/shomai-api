13432c7b7355fe25dbbdbe0cdb0a1f4112687885
TheBoringDude <iamcoderx@gmail.com>
2021-11-01 22:47:58 +0800

    revert to commonjs

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import express from "express";
+const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
   return res.send("API for Shomaii");
 });
 
-export default app;
+module.exports = app;
